096007e4638188762534398296c6239258eb25b6
Alice Vital <alice.nascimento@bsatech.io>
2025-10-13 22:32:59 -0300

    Enviando exercício de organizar portfólio

---
+++ dominar_os_fundamentos/organizando_portfolio.py
@@ -0,0 +1,13 @@
+'''
+Ana está desenvolvendo seu portfólio para exibir os projetos de Python que concluiu.
+Ela organizou uma lista com o nome de cada projeto, mas percebeu que alguns itens podem estar ausentes, aparecendo como None:
+rie um programa que ajude Ana a percorrer a lista de projetos e exiba os nomes dos projetos válidos.
+Se encontrar um item None, o programa deve exibir a mensagem: "Projeto ausente".
+'''
+projetos = ["website", "jogo", "análise de dados", None, "aplicativo móvel"]
+
+for projeto in projetos:
+    if projeto == None:
+        print("Projeto ausente") #se existir um campo none, substitui por essa string
+    else:
+        print(projeto) #se nao, mostra a string da lista
