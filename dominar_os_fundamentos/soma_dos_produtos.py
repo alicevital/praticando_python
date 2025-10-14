fbec920bf3a091d814fa3dce50ce6f1c8e649c99
Alice Vital <alice.nascimento@bsatech.io>
2025-10-13 22:27:14 -0300

    Enviando exercício de soma

---
+++ dominar_os_fundamentos/soma_dos_produtos.py
@@ -0,0 +1,12 @@
+'''
+Você está recebendo uma lista de valores representando os produtos de sua loja virtual e
+gostaria de calcular a soma total desses produtos para entender o desempenho financeiro semanal.
+'''
+
+valores = [10.9, 20.4, 30.45, 40.90, 50.20]
+soma = 0
+
+for valor in valores:
+    soma += valor
+
+print(soma)
